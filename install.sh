1206a1f2e3de9191726ba8cc020923a119d9aecd
sd08012003 <76207887+sd08012003@users.noreply.github.com>
2021-12-01 18:50:54 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,1 @@
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com && pacman-key --lsign-key FBA220DFC880C036 && pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
