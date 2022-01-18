4a6f34d01c329459377c4885ee7d98b8bdb869f3
lucifer1708 <20bce091@nith.ac.in>
2022-01-18 17:20:13 +0530

    shebang 1

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
-pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com && pacman-key --lsign-key FBA220DFC880C036 && pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+#!/bin/bash
+echo "//////////////////////"
+echo "////// step 1 ////////"
+echo "//////////////////////"
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key FBA220DFC880C036
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+
