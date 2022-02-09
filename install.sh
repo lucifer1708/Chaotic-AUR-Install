e7875f0ebb56258dd1f6e9b3ecbdd9d0bd87006c
lucifer1708 <20bce091@nith.ac.in>
2022-02-09 18:24:06 +0530

    new addition

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-echo "//////////////////////"
-echo "////// step 1 ////////"
-echo "//////////////////////"
-pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key FBA220DFC880C036
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+RED='\e[1;31m'
+REDn='\e[0m'
+echo -e "${RED}######################${REDn}"
+echo -e "${RED}          STEP 1      ${RED}"
+echo -e "${RED}######################${REDn}"
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key FBA220DFC880C036
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 echo "//////////////////////////////////////////////"
 sudo sh -c 'echo "[chaotic-aur]" >> /etc/pacman.conf'
