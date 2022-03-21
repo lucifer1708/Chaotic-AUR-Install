ac8180cb45af50a62d0c8e63119ea8318f2ea4d4
Sumit Dhiman <76207887+lucifer1708@users.noreply.github.com>
2022-03-21 22:19:15 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
+
+
+
 RED='\e[1;31m'
 REDn='\e[0m'
-echo -e "${RED}######################${REDn}"
-echo -e "${RED}          STEP 1      ${RED}"
-echo -e "${RED}######################${REDn}"
+
+if [[ "$EUID" = 0 ]]; then
+    continue
+else
+    echo "Please Run As Root"
+    sleep 2
+    exit
+fi
+
+
+echo -e "${RED}##############################################${REDn}"
+echo -e "${RED}          GETTING THE CHAOTIC MIRRORLIST      ${RED}"
+echo -e "${RED}##############################################${REDn}"
 sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key FBA220DFC880C036
-sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+sudo pacman -U --noconfirm --needed 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
-echo -e "${RED}######################${REDn}"
-sudo sh -c 'echo "[chaotic-aur]" >> /etc/pacman.conf'
-sudo sh -c 'echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf'
-echo -e "${RED}########### DONE !! ###########${REDn}"
+echo -e "${RED}##############################################${REDn}"
+#sudo sh -c 'echo "[chaotic-aur]" >> /etc/pacman.conf'
+#sudo sh -c 'echo "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf'
+#echo -e "${RED}########### DONE !! ###########${REDn}"
+
+echo '
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee --append /etc/pacman.conf
